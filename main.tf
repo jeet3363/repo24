4d2c4a210a5a8ae1a3ed04769f681b0e405dd6d1
jitu3363 <jitu3363@yahoo.com>
2026-06-24 16:26:40 +0530

    mera bhai ladkibaj hai

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+required_providers {
+  azurerm = {
+source = "hashicorp/azurerm"
+version = ">4.75.0"
+
+
+  }
+}
+
+
+
+
+
+
+
+
+
+}
+provider "azurerm"{
+
+
+    features {}
+}
+
+
+resource "azurerm_resource_group" "test"{
+
+name = "rg-github"
+location = "westeurope"
+
+
+}
